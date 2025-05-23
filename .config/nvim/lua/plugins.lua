21b020525770da0545bddea7a035cbbfedaee0f9
TsankoTsankov <tzanesh@gmail.com>
2025-05-23 16:52:37 +0300

    added firefox folder and fixed nvim-tree

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -14,7 +14,30 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   -- File explorer
-  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+    {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,  -- Often desired for file explorers so it's ready on startup
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {
+        disable_netrw = true,
+        view = {
+          width = 30,
+        },
+        actions = {
+          open_file = {
+            quit_on_open = true,
+          },
+        },
+        git = {
+          enable = true,
+        },
+      }
+    end,
+  },
+
   -- Fuzzy finder
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
   -- Treesitter
@@ -49,3 +72,38 @@ require("lazy").setup({
   -- Icons
   "nvim-tree/nvim-web-devicons",
 }) 
+
+
+-- Floating tree setup
+local HEIGHT_RATIO = 0.8  -- You can change this
+local WIDTH_RATIO = 0.5   -- You can change this too
+
+require('nvim-tree').setup({
+  view = {
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = 'rounded',
+          relative = 'editor',
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
+  },
+})
---
+++ .mozilla/firefox/hybfwekf.default-release/user.js
@@ -0,0 +1,4 @@
+user_pref("browser.newtab.url", "file:///home/aizen/Documents/www/startpage/startpage.html");
+
+user_pref("browser.startup.homepage", "file:///home/aizen/Documents/www/startpage/startpage.html");
+user_pref("browser.startup.page", 1); // 0: blank, 1: homepage, 3: resume session
